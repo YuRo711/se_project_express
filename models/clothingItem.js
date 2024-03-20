58e0bfb45bfabc95e642c19a5388cc9e073d9d47
YuRo <92213643+YuRo711@users.noreply.github.com>
2024-03-21 01:42:57 +0200

    schemas

---
+++ models/clothingItem.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+const validator = require('validator');
+
+const itemSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 30,
+    },
+    weather: {
+        type: String,
+        enum: ['hot', 'warm', 'cold'],
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        validate: {
+            validator(value) {
+                return validator.isURL(value);
+            },
+            message: 'You must enter a valid URL',
+        },
+    },
+    owner: {
+        type: ObjectId,
+        required: true,
+    },
+    likes: {
+        type: [ObjectId],
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('film', itemSchema);
---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 30,
+    },
+    avatar: {
+        type: String,
+        required: true,
+        validate: {
+            validator(value) {
+                return validator.isURL(value);
+            },
+            message: 'You must enter a valid URL',
+        },
+    },
+});
+
+module.exports = mongoose.model('film', userSchema);
